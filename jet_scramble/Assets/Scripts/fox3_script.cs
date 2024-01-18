035d838d47f4719e63cda974544dc89e105ee265
Joshua Miller <millej23@gmail.com>
2024-01-18 16:47:01 -0500

    Pause on Text Animator - Working on Missle Lock on Texture
    Finish missle lock on

---
+++ jet_scramble/Assets/Scripts/fox3_script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VInspector;
+using VHierarchy;
+using DG.Tweening;
+using TMPro;
+
+public class fox3_script : MonoBehaviour
+{
+
+    public TMP_Text fox_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
