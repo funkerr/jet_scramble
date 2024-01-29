3dceb7bc69562a71f809b295cb3a25da19aca4d8
Joshua Miller <millej23@gmail.com>
2024-01-29 14:33:40 -0500

    Working on Fog and Tweening - not really working
    Not rlly working, go back to something else

--- jet_scramble/Assets/Scripts/fog_settings_script.cs
+++ jet_scramble/Assets/Scripts/fog_settings_script.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 using System.Collections;
+using DG.Tweening;
+using System.Security.Cryptography;
 
 public class fog_settings_script : MonoBehaviour
 {
     public float _myFogDens;
+    public float _myFogDensEnd = .003f;
+    float test;
 
     void Start()
     {
+
+
         RenderSettings.fog = true;
         RenderSettings.fogMode = FogMode.Exponential;
-        RenderSettings.fogDensity = 0.00f;
-        StartCoroutine(updateTheFog());
+        _myFogDens = RenderSettings.fogDensity;
+        Debug.Log("My fog value is: " + _myFogDens);
+        //enderSettings.fogDensity
+        Debug.Log("My fog end value is: " + _myFogDensEnd);
+        StartCoroutine("updateTheFog");
+
+
+
+
+    }
+
+    private void Update()
+    {
+        //_myFogDens = RenderSettings.fogDensity;
+        //DOTween.To(x => RenderSettings.fogDensity = x, 0, 0.1f, 5f);
+
+
+        //RenderSettings.fogDensity = _myFogDens;
+        //Debug.Log("Fog Density is " + RenderSettings.fogDensity);
     }
 
     IEnumerator updateTheFog()
@@ -20,10 +43,16 @@ public class fog_settings_script : MonoBehaviour
             //this makes the loop itself yield 
             yield return new WaitForSeconds(3);
             //testing LERP
-            _myFogDens = Mathf.Lerp(RenderSettings.fogDensity, .002f, .25f) + .001f;
+            Debug.Log("got here");
+            //DOTween.To(x => RenderSettings.fogDensity = x, 0.0f, 0.1f, 10f);
 
-            RenderSettings.fogDensity = _myFogDens;
-            Debug.Log(RenderSettings.fogDensity);
+            // Tween a float called myFloat to 52 in 1 second
+            //DOTween.To(() => RenderSettings.fogDensity, x => RenderSettings.fogDensity = x, .1, 10);
+
+
+            //RenderSettings.fogDensity = _myFogDens;
+            Debug.Log("Fog Density is " + RenderSettings.fogDensity);
+            break;
         }
         //if you want to stop the loop, use: break;
     }
